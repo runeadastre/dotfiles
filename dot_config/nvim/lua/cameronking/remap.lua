5107d6596adafcb4163f41686f202bcbb93b7f9e
Cameron King <c@cameronking.cloud>
2023-04-23 18:10:57 -0700

    Cleanup and isp setup

--- dot_config/nvim/lua/cameronking/remap.lua
+++ dot_config/nvim/lua/cameronking/remap.lua
@@ -1,6 +1,3 @@
--- map leader to space
-vim.g.mapleader = " "
-
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
 -- greatest remap ever
@@ -10,3 +7,13 @@ vim.keymap.set("x", "<leader>p", "\"_dP")
 vim.keymap.set("n", "<leader>y", "\"+y")
 vim.keymap.set("v", "<leader>y", "\"+y")
 vim.keymap.set("n", "<leader>Y", "\"+Y")
+
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+vim.keymap.set("n", "J", "mzJ`z")
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
--- dot_config/nvim/lua/cameronking/set.lua
+++ dot_config/nvim/lua/cameronking/set.lua
@@ -25,3 +25,6 @@ vim.opt.scrolloff = 8
 
 vim.opt.updatetime = 50
 
+vim.opt.colorcolumn = "80"
+
+vim.g.mapleader = " "
